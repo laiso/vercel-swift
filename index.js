0ec305366d5c0ec175463ba6a3116c8785cdf7c0
laiso <laiso@lai.so>
2022-07-30 16:57:03 +0700

    wip

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ exports.version = 3;
 async function build(opts) {
   console.log("swift Building...");
   const lambda = await createLambda({
-    files: {'bootstrap': new FileFsRef({ mode: 0o755, fsPath: 'swift.sh' })},
+    files: {'bootstrap': new FileFsRef({ mode: 0o755, fsPath: '.build/release/MyLambda' })},
+    // files: {'bootstrap': new FileFsRef({ mode: 0o755, fsPath: '.build/arm64-apple-macosx/debug/MyLambda' })},
     handler: 'bootstrap',
-    runtime: 'provided.al2'
+    runtime: 'provided',
   })
   return {
     output: lambda,

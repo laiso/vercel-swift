1a5653f5a933cce3cd84512ae07c73eef9458457
laiso <laiso@lai.so>
2022-07-30 17:15:01 +0700

    wip

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const execa = require('execa');
 const {createLambda, FileFsRef} = require("@vercel/build-utils");
 
 exports.version = 3;
 
 async function build(opts) {
-  console.log("swift Building...");
+  console.log("vercel-swift Building...");
+  await execa(
+      `curl https://download.swift.org/experimental-use-only/repo/amazonlinux/releases/2/swiftlang.repo > /etc/yum.repos.d/swiftlang.repo`,
+      [],
+      { shell: true, stdio: 'inherit' }
+  );
+  await execa(
+      `amazon-linux-extras install epel`,
+      [],
+      { shell: true, stdio: 'inherit' }
+  );
+  await execa(
+      `yum install swiftlang`,
+      [],
+      { shell: true, stdio: 'inherit' }
+  );
+  await execa(
+      `swift build -c release`,
+      [],
+      { shell: true, stdio: 'inherit' }
+  );
   const lambda = await createLambda({
     files: {'bootstrap': new FileFsRef({ mode: 0o755, fsPath: '.build/release/MyLambda' })},
     // files: {'bootstrap': new FileFsRef({ mode: 0o755, fsPath: '.build/arm64-apple-macosx/debug/MyLambda' })},

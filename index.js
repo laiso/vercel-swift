2833925cbef20953a96ba9341120af879e212ae4
laiso <laiso@lai.so>
2022-07-30 16:01:16 +0700

    wip

---
+++ index.js
@@ -0,0 +1,23 @@
+const {createLambda, FileFsRef} = require("@vercel/build-utils");
+
+exports.version = 3;
+
+async function build(opts) {
+  console.log("swift Building...");
+  const lambda = await createLambda({
+    files: {'bootstrap': new FileFsRef({ mode: 0o755, fsPath: 'swift.sh' })},
+    handler: 'bootstrap',
+    runtime: 'provided.al2'
+  })
+  return {
+    output: lambda,
+    watch: [
+      // Dependent files to trigger a rebuild in `vercel dev` go here…
+    ],
+    routes: [
+      // If your Runtime needs to define additional routing, define it here…
+    ],
+  };
+}
+
+exports.build = build;
